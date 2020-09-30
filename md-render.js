12c1f5ccb8a08b8e2d2cd9a4365a425ea386086e
Rich <rjc@mit.edu>
2020-09-30 13:01:39 -0400

    moved event handler assignment into md descriptor's observe() function - sets focusIn handler and keydown handlers + focusin handler recalculates when result gains focus using code from previousElementSibling + keyboard handler recalculcates when focus is in source element when control+enter is pressed - when source element has focus, related result gets role of "status", otherwise role is removed + this causes control+enter to speak result immediately while focus remains in source element

--- md-render.js
+++ md-render.js
@@ -3,17 +3,17 @@ import purify from "./purify.es.js";
 
 // start at heading level 2
 const walkTokens = (token) => {
-  if (token.type === 'heading') {
-    token.depth += 1;
-  } // if
-    } // walkTokens
-  
+if (token.type === 'heading') {
+token.depth += 1;
+} // if
+} // walkTokens
+
 const renderer = {
 code(code, type, escaped) {
 switch (type.toLowerCase()) {
-	case "html": return this.processCode(code, type, escaped);
+case "html": return this.processCode(code, type, escaped);
 case "js": return this.processCode(code, type, escaped);
-		default: return false;
+default: return false;
 } // switch
 }, // code
 
@@ -23,27 +23,45 @@ processCode: processCode
 marked.use({walkTokens, renderer});
 
 const MdRender = {
-options: {
-	connect: (host, key) => {
-		try { host[key] = JSON.parse(host.getAttribute(key));}
-		catch (e) {console.error(e);}
-	} // connect
+/*options: {
+connect: (host, key) => {
+try { host[key] = JSON.parse(host.getAttribute(key));}
+catch (e) {console.error(e);}
+} // connect
 }, // options
+*/
 
+result: ref(".md-result"),
 
 md: {
-	get: (host, value) => host.innerHTML
+get: (host, value) => host.innerHTML,
+observe: (host, value) => {
+console.debug("assigning event listeners...");
+if (value) {
+host.result.addEventListener("focusin", resultFocusHandler);
+host.result.addEventListener("keydown", sourceKeyboardHandler);
+} // if
+} // observe
 }, // md
 
 render: render(({ md }) => {
 return (host) => host.innerHTML = 
-`<div>${marked(host.innerHTML)}</div>`;
+`<div class="md-result">${marked(host.innerHTML)}</div>`;
 }, {shadowRoot: false}) // render
 }; // descriptors
 
 define("md-render", MdRender);
 
-document.addEventListener("focusout",  redisplayResult);
+export function ref(query) {
+return ({ render }) => {
+if (typeof render === 'function') {
+const target = render();
+return target.querySelector(query);
+} // if
+
+return null;
+}; // return function
+} // ref
 
 function* idGen (name) {
 const map = new Map();
@@ -58,8 +76,8 @@ const jsId = idGen("jsCodeBlock");
 
 function processCode  (code, type, escaped) {
 return `<div class="code-container ${type}">
-<div class="source ${type}" contenteditable data-code='${code}'>${this.code(code, "_original_", escaped)}</div>
-<div class="result ${type}" tabindex="0">${processResult(code, type, escaped)}</div>
+<div class="source" data-type="${type}" contenteditable data-code='${code}'>${this.code(code, "_original_", escaped)}</div>
+<div class="result" aria-atomic="true" data-type="${type}" tabindex="0">${processResult(code, type, escaped)}</div>
 </div>`;
 } // processCode
 
@@ -70,24 +88,41 @@ return type === "html"? code
 
 function processJavascript (code, escaped) {
 try {
-	const fun = new Function("", code);
+const fun = new Function("", code);
 //fun.name = jsId.next().value;
 //console.debug("fun: ", fun);
 const result = fun();
 
 return result;
 } catch (e) {
-	return e.message;
+return e.message;
 } // try
 } // processJavascript
 
-function redisplayResult (event) {
-	const source  = event.target;
-const result = source.nextElementSibling;
+function resultFocusHandler (e) {
+const result = e.target;
+const source = e.relatedTarget;
+if (result.classList?.contains("result") && source.classList?.contains("source") && source === result.previousElementSibling) {
 const code = source.textContent;
-//console.debug("redisplay: ", source, result, code);
+result.removeAttribute("role");
+result.textContent = processResult(code, source.dataset.type);
+} // if
+} // resultFocusHandler
 
-if (source.classList.contains("js")) result.textContent = processJavascript(code);
-else if (source.classList.contains("html")) result.innerHTML = code;
-} // redisplayResult
+function sourceKeyboardHandler (e) {
+const source = e.target;
+if (!source.classList?.contains("source")) return;
+const result  = e.target.nextElementSibling;
+if (!result.classList?.contains("result")) return;
 
+result.setAttribute("role", "status");
+
+switch (e.key) {
+case "Enter": if (e.ctrlKey) {
+e.preventDefault();
+const code = e.target.textContent;
+result.textContent = processResult(code, source.dataset.type);
+} // if
+} // switch
+
+} // sourceKeyboardHandler
